c7d3b77392bafbf03bef71fed22b9ee8dd535a19
Anne LoVerso <anne@anne.(none)>
2014-10-06 16:24:59 -0400

    working with twist angles

--- mobile_robotics/scripts/marco_polo.py
+++ mobile_robotics/scripts/marco_polo.py
@@ -4,6 +4,8 @@ import rospy
 import math
 import tf
 import geometry_msgs.msg
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Vector3
 
 class Robot:
     def __init__(self,robotname):
@@ -13,6 +15,7 @@ class Robot:
         self.pub = rospy.Publisher("/%s/cmd_vel"%self.robotname, Twist, queue_size=10)
 
     def run(self):
+        #print "running"
         self.broadcaster.sendTransform(
             (0.33655, 0.0, 0.0), 
             (0, 0, 0, 1),
@@ -36,16 +39,21 @@ class Marco(Robot):
     def __init__(self, polo_names):
         Robot.__init__(self, "marco")
         self.polo_names = polo_names
-        self.last_call_time = 0
+        self.last_call_time = rospy.get_time()
+        self.startup_time = rospy.get_time()
 
     def run(self):
         Robot.run(self)
-        angle_to_polo = 0
-        if rospy.Time(0) - self.last_call_time >= 5: #call every 5 seconds
-            self.last_call_time = rospy.Time(0)
-            angle_to_polo = self.call_marco()
-        Robot.publish_twist_velocity(self, 0.2, angle_to_polo)
-        
+        if (rospy.get_time() - self.startup_time >= 5):
+            angle_to_polo = 0
+            #print rospy.get_time()
+            #rospy.Timer(rospy.Duration(5), my_callback)
+            #print now.secs
+            if rospy.get_time() - self.last_call_time >= 5: #call every 5 seconds
+                self.last_call_time = rospy.get_time()
+                angle_to_polo = self.call_marco()
+            #Robot.publish_twist_velocity(self, 0, angle_to_polo)
+            
     def call_marco(self):
         closest_polo_dist = 1000000
         angle_to_polo = 0
@@ -72,24 +80,26 @@ class Polo(Robot):
         Robot.run(self)
         # find where marco is relative to self
         angular = 0
+        radius = 1
         try:
             (trans,rot) = self.listener.lookupTransform(
                 "/%s/base_link"%self.robotname, 
                 "/marco/base_link",
                  rospy.Time(0))
-            radius = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            print tf.transformations.euler_from_quaternion(rot)
+            #radius = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
             angular = math.atan2(trans[1], trans[0])
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             pass
         coef = (1 if angular > 0 else -1)
         angle_away_from_marco = angular + 3.14159*coef # 180 degrees away from where it sense marco
         speed = 0.5/radius # the closer marco is, the faster polo moves
-        Robot.publish_twist_velocity(self, speed, angle_away_from_marco) 
+        #Robot.publish_twist_velocity(self, speed, angle_away_from_marco) 
 
 if __name__ == '__main__':
     rospy.init_node('marco_polo', anonymous=True)
 
-    # polo_names = ["polo1", "polo2"]
+    #polo_names = ["polo1", "polo2"]
     polo_names = ["polo"]
     marco = Marco(polo_names)
     polo = Polo("polo")
